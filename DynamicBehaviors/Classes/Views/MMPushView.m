7d0503ae062f81267b3c9e098dcbac255f63cdca
刘春牢 <liuchunlao@qq.com>
2016-06-01 14:47:53 +0800

    09-推动行为02
    1.画线

--- DynamicBehaviors/Classes/Views/MMPushView.m
+++ DynamicBehaviors/Classes/Views/MMPushView.m
@@ -9,8 +9,15 @@
 #import "MMPushView.h"
 @interface MMPushView () {
     
-    // 小图片
+    /**
+     *  小图片
+     */
     UIImageView *_imgView;
+    
+    /**
+     *  当前点
+     */
+    CGPoint _currenP;
 
 }
 
@@ -27,7 +34,6 @@
         // MARK: - 1.图片框
         UIImageView *imgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"AttachmentPoint_Mask"]];
         
-        imgView.center = self.center;
         imgView.hidden = YES;
         
         [self addSubview:imgView];
@@ -57,14 +63,51 @@
         _imgView.hidden = NO;
         
     } else if (recognizer.state == UIGestureRecognizerStateChanged) {
+        
         // 2.2 移动
+        // - 记录当前点
+        _currenP = loc;
+        
+        // - 重绘
+        [self setNeedsDisplay];
         
         
     } else if (recognizer.state == UIGestureRecognizerStateEnded) {
         // 2.3 结束
         _imgView.hidden = YES;
+        
+        // - 清除线
+        _imgView.center = CGPointZero;
+        _currenP = CGPointZero;
+        
+        [self setNeedsDisplay];
+        
     }
 
+
+}
+
+#pragma mark - 进行绘图操作
+- (void)drawRect:(CGRect)rect {
+    
+    // 1.创建路径对象
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    
+    // 1.1 设置起点
+    [path moveToPoint:_imgView.center];
+    
+    // 1.2 添加线
+    [path addLineToPoint:_currenP];
+    
+    // 1.3 线宽及线头样式
+    path.lineWidth = 10;
+    path.lineCapStyle = kCGLineCapRound;
+    
+    
+    [[UIColor colorWithRed:arc4random_uniform(256)/255.0 green:arc4random_uniform(256)/255.0  blue:arc4random_uniform(256)/255.0  alpha:1.0] setStroke];
+    
+    // 2.渲染
+    [path stroke];
 
 }
 
