9c6ad66ccfa882d6bdcd126d559582e1593b842f
刘春牢 <liuchunlao@qq.com>
2016-06-01 10:41:07 +0800

    01-项目准备
    1.加载自定义控制器
    2.显示列表数据

---
+++ DynamicBehaviors/Classes/Controllers/MMListController.m
@@ -0,0 +1,74 @@
+//
+//  MMListController.m
+//  DynamicBehaviors
+//
+//  Created by Apple on 16/6/1.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import "MMListController.h"
+
+@interface MMListController () {
+    // 下划线的成员变量!
+    // 所有行为的集合
+    NSArray *_behaviorsArr;
+
+}
+
+@end
+
+@implementation MMListController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // 1.设置标题
+    // 直接设置title -> 导航栏的标题 及 tabBarItem的title同时设置!
+    self.title = @"仿真行为";
+    
+    // 2.设置集合数据
+    _behaviorsArr = @[
+                      @"吸附行为",
+                      @"推动行为",
+                      @"刚性附着行为",
+                      @"弹性附着行为",
+                      @"碰撞检测"
+                      ];
+    
+    // 3.不显示多余的行
+    self.tableView.tableFooterView = [[UIView alloc] init];
+}
+
+
+#pragma mark - 数据源方法
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return _behaviorsArr.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    // 1.cell
+    static NSString *ID = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+        
+        // 设置箭头!
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    // 2.数据
+    cell.textLabel.text = _behaviorsArr[indexPath.row];
+    
+    // 3.返回
+    return cell;
+
+}
+
+@end
