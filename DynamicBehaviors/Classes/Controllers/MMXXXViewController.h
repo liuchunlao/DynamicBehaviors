c13480b2fa396991137de64c425d2cb80d9bbbf1
liuchunlao <liuchunlao@qq.com>
2016-06-03 16:49:07 +0800

    创建了xxx类

---
+++ DynamicBehaviors/Classes/Controllers/MMXXXViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMXXXViewController.h
+//  DynamicBehaviors
+//
+//  Created by Apple on 16/6/3.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMXXXViewController : UIViewController
+
+@end
